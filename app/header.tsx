8cc64f79320c4a81acda4571f68923d5cb7b48eb
Camilo Reyes <xikmiloix.com@gmail.com>
2024-12-05 10:12:49 -0500

    Initial project

---
+++ app/header.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+const Header = () => {
+    return (
+        <div className="flex bg-black justify-between items-center px-7 py-5">
+            <div className="flex gap-4">
+                <Image src="/logo.png" alt="Vercel Logo" width={100} height={24} className="ml-6"/>
+                <a href="" className="text-white px-4">Popular</a>
+                <a href="" className="text-white px-4">Favorites</a>
+            </div>
+            <div className="flex">
+                <Image src="/login.svg" alt="Vercel Logo" width={30} height={24} priority/>
+            </div>
+        </div>
+    )
+};
+
+export default Header;
---
+++ app/movies/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Banner from "./banner";
+
+export default function Home() {
+  return (
+    <div className="w-full">
+      <Banner/>
+    </div>
+  );
+}
